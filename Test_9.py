4fd8581cbd1aede3d1664cf37b5516fbc281bd9f
Enotlate <106143455+Enotlate@users.noreply.github.com>
2024-01-12 04:19:13 +0300

    Add pagination

--- Test_9.py
+++ Test_9.py
@@ -68,6 +68,114 @@ payload_6 = {
     }
 }
 
+payload_7 = {
+    "version": "1.0",
+    "filter": {
+        "fgit": {
+            "hasGit": True
+        }
+    },
+    "pagination" : {
+        "count" : 20,
+        "number" : 1,
+        "random" : "false",
+        "sort" : "lastname",
+        "orderDesc": "false"
+        }
+}
+
+payload_8 = {
+    "version": "1.0",
+        "filter": {
+            "fgit": {
+                "hasGit": False
+        }
+    },
+    "pagination" : {
+        "count" : 20,
+        "number" : 1,
+        "random" : "false",
+        "sort" : "lastname",
+        "orderDesc": "false"
+        }
+}
+
+payload_9 = {
+    "version": "1.0",
+    "filter": {
+        "fgit": {
+            "hasGit": True
+        },
+        "fcontact": {
+            "hasContact": True
+        }
+    },
+        "pagination" : {
+            "count" : 20,
+            "number" : 1,
+            "random" : "false",
+            "sort" : "lastname",
+            "orderDesc": "false"
+        }
+}
+
+payload_10 = {
+    "version": "1.0",
+    "filter": {
+        "fgit": {
+            "hasGit": False
+        },
+        "fcontact": {
+            "hasContact": False
+        }
+    },
+        "pagination" : {
+            "count" : 20,
+            "number" : 1,
+            "random" : "false",
+            "sort" : "lastname",
+            "orderDesc": "false"
+        }
+}
+
+payload_11 = {
+    "version": "1.0",
+    "filter": {
+        "fgit": {
+            "hasGit": True
+        },
+        "fcontact": {
+            "hasContact": False
+        }
+    },
+        "pagination" : {
+            "count" : 20,
+            "number" : 1,
+            "random" : "false",
+            "sort" : "lastname",
+            "orderDesc": "false"
+        }
+}
+
+payload_12 = {
+    "version": "1.0",
+    "filter": {
+        "fgit": {
+            "hasGit": False
+        },
+        "fcontact": {
+            "hasContact": True
+        }
+    },
+        "pagination" : {
+            "count" : 20,
+            "number" : 1,
+            "random" : "false",
+            "sort" : "lastname",
+            "orderDesc": "false"
+        }
+}
+
 BASE_URL = 'http://192.168.0.16:8080/students/general/get-student-list'
 
 def test_hasGit_true():
@@ -134,6 +242,71 @@ def test_hasGit_false_and_hasContact_true():
         assert student["fcontact"]["hasContact"] == True, "Поле контакты пустое"
     print("test_hasGit_false_and_hasContact_true completed successfully in", end_time-start_time, "seconds")
 
+def test_hasGit_true_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_7)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == True, "Поле git пустое"
+    print("test_hasGit_true_pagination completed successfully in", end_time-start_time, "seconds")
+
+def test_hasGit_false_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_8)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == False, "Поле git не пустое"
+    print("test_hasGit_false_pagination completed successfully in", end_time-start_time, "seconds")
+
+def test_hasGit_true_and_hasContact_true_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_9)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == True, "Поле git пустое"
+        assert student["fcontact"]["hasContact"] == True, "Поле контакты пустое"
+    print("test_hasGit_true_and_hasContact_true_pagination completed successfully in", end_time-start_time, "seconds")
+
+def test_hasGit_false_and_hasContact_false_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_10)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == False, "Поле git не пустое"
+        assert student["fcontact"]["hasContact"] == False, "Поле контакты не пустое"
+    print("test_hasGit_false_and_hasContact_false_pagination completed successfully in", end_time-start_time, "seconds")
+
+def test_hasGit_true_and_hasContact_false_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_11)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == True, "Поле git пустое"
+        assert student["fcontact"]["hasContact"] == False, "Поле контакты не пустое"
+    print("test_hasGit_true_and_hasContact_false_pagination completed successfully in", end_time-start_time, "seconds")
+
+def test_hasGit_false_and_hasContact_true_pagination():
+    start_time = time.time()
+    response = requests.post(f'{BASE_URL}', json=payload_12)
+    response_body = response.json()
+    end_time = time.time()
+    assert response.status_code == 200
+    for student in response_body["info"]:
+        assert student["fgit"]["hasGit"] == False, "Поле git не пустое"
+        assert student["fcontact"]["hasContact"] == True, "Поле контакты пустое"
+    print("test_hasGit_false_and_hasContact_true_pagination completed successfully in", end_time-start_time, "seconds")
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
     test_hasGit_true()
@@ -142,3 +315,9 @@ if __name__ == "__main__":
     test_hasGit_false_and_hasContact_false()
     test_hasGit_true_and_hasContact_false()
     test_hasGit_false_and_hasContact_true()
+    test_hasGit_true_pagination()
+    test_hasGit_false_pagination()
+    test_hasGit_true_and_hasContact_true_pagination()
+    test_hasGit_false_and_hasContact_false_pagination()
+    test_hasGit_true_and_hasContact_false_pagination()
+    test_hasGit_false_and_hasContact_true_pagination()
